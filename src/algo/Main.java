9e71204fcc57c2e4e93ff35fdb1be678bd070fff
acleret69 <aldrickcleret@gmail.com>
2022-01-09 17:07:14 +0100

    TD2

--- src/algo/Main.java
+++ src/algo/Main.java
@@ -3,6 +3,21 @@ package algo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("changer de branche en fonction des TD et exos ");
+        int firstArray[] = {25, 40, 2, 65, 10};
+        int secondArray[] = {10, 50, 1, 25, 35, 15};
+
+        System.out.print("Tableau avant le tri par tas: \t");
+        HeapSort.print(firstArray);
+        HeapSort.sort(firstArray);
+
+        System.out.print("Tableau après le tri par tas: \t");
+        HeapSort.print(firstArray);
+
+        System.out.print("\nTableau avant le tri par tas: \t");
+        HeapSort.print(secondArray);
+        HeapSort.sort(secondArray);
+
+        System.out.print("Tableau après le tri par tas: \t");
+        HeapSort.print(secondArray);
     }
 }
