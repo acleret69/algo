a815deaa63ad2394793860ae08b558b9e892927e
acleret69 <aldrickcleret@gmail.com>
2022-01-09 17:15:21 +0100

    TD4 exo2

--- src/algo/Main.java
+++ src/algo/Main.java
@@ -4,20 +4,15 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        List<Integer> array = new ArrayList<>();
         BinarySearchTree binarySearchTree = new BinarySearchTree();
-        binarySearchTree.insert(new BinarySearchNode(20));
-        binarySearchTree.insert(new BinarySearchNode(5));
-        binarySearchTree.insert(new BinarySearchNode(25));
-        binarySearchTree.insert(new BinarySearchNode(3));
-        binarySearchTree.insert(new BinarySearchNode(12));
-        binarySearchTree.insert(new BinarySearchNode(21));
-        binarySearchTree.insert(new BinarySearchNode(28));
-        binarySearchTree.insert(new BinarySearchNode(8));
-        binarySearchTree.insert(new BinarySearchNode(13));
-        binarySearchTree.insert(new BinarySearchNode(6));
 
-        System.out.println("Hauteur de la racine de l'arbre binaire de recherche: " + BinarySearchNode.getHeight(binarySearchTree.getRoot()));
-        System.out.println("Facteur d'equilibrage de la racine de l'arbre binaire de recherche: " + BinarySearchNode.balanceFactor(binarySearchTree.getRoot()));
-        System.out.println("Cet arbre est un arbre equilibre ? " + binarySearchTree.isBalanceTree());
+        array = Arrays.asList(10, 5, 12, 2, 7, 15, 4, 17);
+        binarySearchTree.listToTree(array);
+        BinarySearchTree.print(binarySearchTree.getRoot().getRightChild(), "-");
+        System.out.println("Facteur d'equilibrage du fils droit de la racine avant rotation simple gauche: " + BinarySearchNode.balanceFactor(binarySearchTree.getRoot().getRightChild()) + "\n");
+
+        BinarySearchTree.print(BinarySearchNode.simpleLeftRotate(binarySearchTree.getRoot().getRightChild()), "-");
+        System.out.println("Facteur d'equilibrage du fils droit de la racine après rotation simple gauche: " + BinarySearchNode.balanceFactor(BinarySearchNode.simpleLeftRotate(binarySearchTree.getRoot().getRightChild())));
     }
 }
