dd8c06cd50667b8923b79501e8420c22f0e656c7
acleret69 <aldrickcleret@gmail.com>
2022-01-09 22:19:41 +0100

    TD1-exo2

--- src/algo/Main.java
+++ src/algo/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         Node<String> A = new Node<>("a");
-
         Node<String> B = A.addChild(new Node<>("b"));
         Node<String> C = A.addChild(new Node<>("c"));
         Node<String> D = A.addChild(new Node<>("d"));
@@ -24,14 +23,6 @@ public class Main {
         Node<String> P = M.addChild(new Node<>("p"));
         Tree tree = new Tree(A);
 
-        System.out.println("Parcours pour l'arbre de l'exercice 1");
-        System.out.print("Parcours en largeur: \t\t\t\t");
-        Tree.widthCourse(tree.getRoot());
-        System.out.print("\nParcours en profondeur prefixe: \t");
-        Tree.prefixDepthCourse(tree.getRoot(), new ArrayList<>());
-        System.out.print("\nParcours en profondeur suffixe: \t");
-        Tree.postfixDepthCourse(tree.getRoot(), new ArrayList<>());
-
         BinaryNode<Integer> i20 = new BinaryNode<>(20);
         BinaryNode<Integer> i5 = i20.setLeftChild(new BinaryNode<>(5));
         BinaryNode<Integer> i25 = i20.setRightChild(new BinaryNode<>(25));
@@ -44,14 +35,22 @@ public class Main {
         BinaryNode<Integer> i6 = i8.setLeftChild(new BinaryNode<>(6));
         BinaryTree binaryTree = new BinaryTree(i20);
 
-        System.out.println("\nParcours pour l'arbre de l'exercice 2");
-        System.out.print("Parcours en largeur: \t\t\t\t");
+        System.out.println("Parcours arbre 1");
+        System.out.print("Parcours en largeur: ");
+        Tree.widthCourse(tree.getRoot());
+        System.out.print("\nprefixe: ");
+        Tree.prefixDepthCourse(tree.getRoot(), new ArrayList<>());
+        System.out.print("\nsuffixe: ");
+        Tree.postfixDepthCourse(tree.getRoot(), new ArrayList<>());
+        System.out.println("\n---------------------------");
+        System.out.println("Parcours arbre 2");
+        System.out.print("Parcours en largeur: ");
         BinaryTree.widthCourse(binaryTree.getRoot());
-        System.out.print("\nParcours en profondeur prefixe: \t");
+        System.out.print("\nprefixe: ");
         System.out.println(binaryTree.prefixDepthCourse(binaryTree.getRoot()));
-        System.out.print("Parcours en profondeur suffixe: \t");
+        System.out.print("suffixe: ");
         System.out.println(binaryTree.postfixDepthCourse(binaryTree.getRoot()));
-        System.out.print("Parcours en profondeur infixe: \t\t");
+        System.out.print("infixe: ");
         System.out.println(binaryTree.infixDepthCourse(binaryTree.getRoot()));
     }
 }
